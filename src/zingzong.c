9dac9d2a7cf042b9058760594a68776812c982dd
Ben G. Han <benjihan@users.sourceforge.net>
2017-06-14 19:47:59 +0200

    Fix licence and copyrights.

--- src/zingzong.c
+++ src/zingzong.c
@@ -3,12 +3,40 @@
  * @data   2017-06-06
  * @author Benjamin Gerard
  * @brief  a simple quartet music player.
+ *
+ * ----------------------------------------------------------------------
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 Benjamin Gerard AKA Ben/OVR.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 static const char copyright[] = \
   "Copyright (c) 2017 Bejamin Gerard AKA Ben/OVR";
-static const char bugreport[] = \
-  "Report bugs to <>";
+static const char licence[] = \
+  "Licenced under MIT licence";
+static const char bugreport[] =                                 \
+  "Report bugs to <https://github.com/benjihan/zingzong>";
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -777,6 +805,7 @@ static void print_usage(void)
     );
     puts("");
     puts(copyright);
+    puts(licence);
     puts(bugreport);
 }
 
@@ -787,6 +816,7 @@ static void print_version(void)
 {
   puts(PACKAGE_STRING "\n");
   puts(copyright);
+  puts(licence);
 }
 
 /**
