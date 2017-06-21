7eff0d2da326bb1484b3011aff5b5439833abfb3
Ben G. Han <benjihan@users.noreply.github.com>
2017-06-22 00:49:45 +0200

    Fix tag list dort order.

--- src/vcversion.sh
+++ src/vcversion.sh
@@ -12,7 +12,7 @@ else
     if [ -r ./VERSION ]; then
 	cat ./VERSION
     else
-	tagname=$(git tag -l 'v[0-9]*' --sort=tag -i --no-column | head -n1)
+	tagname=$(git tag -l 'v[0-9]*' --sort=-tag -i --no-column | head -n1)
 	tweaks=$(git rev-list --count HEAD ^$tagname)
 	if [ "${tweaks}" = 0 ]; then
 	    tweaks=''
