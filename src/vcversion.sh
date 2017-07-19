bdaa84589d025d9cb4c4144d2310a44c3e0245a0
Ben G. Han <benjihan@users.noreply.github.com>
2017-07-19 13:36:05 +0200

    Fix tag sorting key.

--- src/vcversion.sh
+++ src/vcversion.sh
@@ -2,6 +2,10 @@
 #
 # by Benjamin Gerard
 #
+# Count git commit since last tag (AKA TWEAK number)
+#
+# ----------------------------------------------------------------------
+#
 
 export LC_ALL=C
 
@@ -13,7 +17,7 @@ else
 	cat ./VERSION
     else
 	which git >/dev/null
-	tagname=$(git tag -l 'v[0-9]*' --sort=-tag | head -n1)
+	tagname=$(git tag -l 'v[0-9]*' --sort=-v:refname | head -n1)
 	test "x$tagname" != x
 	tweaks=$(git rev-list --count HEAD ^$tagname)
 	if [ "${tweaks}" = 0 ]; then
